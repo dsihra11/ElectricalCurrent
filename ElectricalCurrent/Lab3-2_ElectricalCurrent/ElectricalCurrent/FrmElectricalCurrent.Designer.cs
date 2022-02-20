2008e521b2b2b9cc6aea125207b48dd9cbe4f161
dsihra11 <devonsihra@hotmail.com>
2022-02-19 23:22:44 -0500

    Initial Commit

---
+++ ElectricalCurrent/Lab3-2_ElectricalCurrent/ElectricalCurrent/FrmElectricalCurrent.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ElectricalCurrent
+{
+    partial class FrmElectricalCurrent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBatteryVoltage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResistance = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCapacitance = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Battery Voltage (volts):";
+            // 
+            // txtBatteryVoltage
+            // 
+            this.txtBatteryVoltage.Location = new System.Drawing.Point(147, 6);
+            this.txtBatteryVoltage.Name = "txtBatteryVoltage";
+            this.txtBatteryVoltage.Size = new System.Drawing.Size(100, 20);
+            this.txtBatteryVoltage.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resistor Value (ohms):";
+            // 
+            // txtResistance
+            // 
+            this.txtResistance.Location = new System.Drawing.Point(147, 37);
+            this.txtResistance.Name = "txtResistance";
+            this.txtResistance.Size = new System.Drawing.Size(100, 20);
+            this.txtResistance.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Capacitance (farads):";
+            // 
+            // txtCapacitance
+            // 
+            this.txtCapacitance.Location = new System.Drawing.Point(147, 68);
+            this.txtCapacitance.Name = "txtCapacitance";
+            this.txtCapacitance.Size = new System.Drawing.Size(100, 20);
+            this.txtCapacitance.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Time (seconds):";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(147, 99);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ElectricalCurrent.Properties.Resources.ElectricalCircuit;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Current (amps):";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCurrent.Location = new System.Drawing.Point(129, 249);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(100, 23);
+            this.lblCurrent.TabIndex = 10;
+            this.lblCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(11, 285);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(74, 27);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Location = new System.Drawing.Point(100, 285);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 27);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(189, 285);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(74, 27);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmElectricalCurrent
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(275, 324);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCapacitance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResistance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBatteryVoltage);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmElectricalCurrent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculate Electrical Current";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBatteryVoltage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResistance;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCapacitance;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ElectricalCurrent/Lab3-2_ElectricalCurrent/ElectricalCurrent/FrmElectricalCurrent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Devon Sihra
+namespace ElectricalCurrent
+{
+    public partial class FrmElectricalCurrent : Form
+    {
+        
+       
+
+        public FrmElectricalCurrent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double voltage = double.Parse(txtBatteryVoltage.Text);
+            double res = double.Parse(txtResistance.Text);
+            double cap = double.Parse(txtCapacitance.Text);
+            double time = double.Parse(txtTime.Text);
+            double exp = -time / (res * cap);
+            //processing 
+            double current = voltage * Math.Pow(2.718, exp)/res;
+            lblCurrent.Text = current.ToString("E2");
+        
+
+            
+             
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBatteryVoltage.Text = "0";
+            txtCapacitance.Text = "0";
+            txtResistance.Text = "0";
+            txtTime.Text = "0";
+
+            lblCurrent.Text = "0";
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
